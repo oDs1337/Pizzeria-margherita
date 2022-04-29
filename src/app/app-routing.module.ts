fd3a75cfa2c5583f2ed10e7948ed61071d4498f0
oDs <odstobiaszcod@gmail.com>
2022-04-29 11:00:51 +0200

    Add routing paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { ShipComponent } from './ship/ship.component';
+import { OrderComponent } from './order/order.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'order', component: OrderComponent },
+  { path: 'ship', component: ShipComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

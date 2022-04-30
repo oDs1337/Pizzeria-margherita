6276f3734092ce526115f8e6860dbf28f60b4131
oDs <odstobiaszcod@gmail.com>
2022-04-30 17:44:24 +0200

    Working guard version

--- src/app/admin.guard.ts
+++ src/app/admin.guard.ts
@@ -9,7 +9,8 @@ export class AdminGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+
+    return false;
   }
-  
+
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AdminGuard } from './admin.guard';
 import { ShipComponent } from './ship/ship.component';
 import { OrderComponent } from './order/order.component';
 import { RegisterComponent } from './register/register.component';
@@ -8,7 +9,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'order', component: OrderComponent },
+  { path: 'order', component: OrderComponent, canActivate: [AdminGuard] },
   { path: 'ship', component: ShipComponent },
 ];
 

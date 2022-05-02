62941c37f9fef72c51058d946414ae0265e79055
oDs <odstobiaszcod@gmail.com>
2022-05-02 11:55:01 +0200

    Working guest guard which disables login component when user is logged in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { GuestGuard } from './guest.guard';
 import { AdminGuard } from './admin.guard';
 import { ShipComponent } from './ship/ship.component';
 import { OrderComponent } from './order/order.component';
@@ -7,7 +8,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginComponent, canActivate: [GuestGuard] },
   { path: 'register', component: RegisterComponent },
   { path: 'order', component: OrderComponent, canActivate: [AdminGuard] },
   { path: 'ship', component: ShipComponent },
--- src/app/guest.guard.ts
+++ src/app/guest.guard.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AccountService } from './account.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GuestGuard implements CanActivate {
+
+  constructor(private accountService: AccountService) { }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+
+    return !this.accountService.isLoggedIn;
   }
-  
+
 }
